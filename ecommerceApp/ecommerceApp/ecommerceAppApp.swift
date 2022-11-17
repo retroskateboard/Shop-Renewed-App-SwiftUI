f1d68ce1802c2c6e79e940dc3d57573303b4eaaf
Eric Rojas Pech <ericrokerz@gmail.com>
2022-11-16 21:06:44 -0600

    creación
    primer commit con el proyecto en ceros

---
+++ ecommerceApp/ecommerceApp/ecommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecommerceAppApp.swift
+//  ecommerceApp
+//
+//  Created by Eric Rojas Pech on 16/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ecommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
